b99d1022cc1f8f16f21165cf1f3e0f2f506ce0ea
sunliguo <sunliguo2006@qq.com>
2022-06-12 06:50:55 +0800

    抄写了几个例子

---
+++ Python编程从入门到实践/chap8/pizza.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+'''
+ @Time : 2022/6/12 6:30
+ @Author : sunliguo
+ @Email : sunliguo2006@qq.com
+ @File : pizza.py
+ @Project : learning-python
+'''
+def make_pizza(*toppings):
+    '''
+    打印顾客点的所有配料。
+    '''
+    print(toppings)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms','green peppers','extra cheese')
