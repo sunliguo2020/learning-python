65f3ef76a680e1739293a4ba021f44f8a24c105e
sunliguo <sunliguo2006@qq.com>
2022-06-12 21:46:36 +0800

    整理文件

--- Python编程从入门到实践/chap08/formatted_name.py
+++ Python编程从入门到实践/chap08/formatted_name.py
@@ -4,7 +4,9 @@
 @contact: QQ376440229
 @Created on: 2022/1/16 8:35
 """
-def get_formatted_name(first_name,last_name):
+
+
+def get_formatted_name(first_name, last_name):
     """
     返回整洁的姓名
     :param first_name:
@@ -12,7 +14,8 @@ def get_formatted_name(first_name,last_name):
     :return:
     """
     full_name = f"{first_name} {last_name}"
-    return  full_name.title()
+    return full_name.title()
 
-musician = get_formatted_name('jimi','hendrix')
+
+musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
