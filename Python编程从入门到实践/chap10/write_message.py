8c7da44d95c2bfd182332a48d796ac0361729c4b
sunliguo <sunliguo2006@qq.com>
2022-06-12 16:58:43 +0800

    s3 笔记本提交

---
+++ Python编程从入门到实践/chap10/write_message.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2021/2/11 21:45
+"""
+filename = 'programming.txt'
+
+with open(filename,'w') as file_object:
+    file_object.write("I love programming.")
+    file_object.write("I love creating new games.")
