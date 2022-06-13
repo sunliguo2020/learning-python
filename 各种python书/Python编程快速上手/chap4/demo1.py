f81f39aaa082eeed673eb6734b96ffdaecac8ddf
sunliguo <sunliguo2006@qq.com>
2022-06-13 19:55:00 +0800

    python代码美化

--- 各种python书/Python编程快速上手/chap4/demo1.py
+++ 各种python书/Python编程快速上手/chap4/demo1.py
@@ -4,10 +4,10 @@
 @contact: QQ376440229
 @Created on: 2020/11/19 21:01
 """
-spam =42
-print("id(spam)",id(spam))
+spam = 42
+print("id(spam)", id(spam))
 chess = spam
-print("id(chess)",id(chess))
+print("id(chess)", id(chess))
 spam = 1000
-print("id(spam),spam)",id(spam),spam)
-print("id(chess),chess",id(chess),chess)
+print("id(spam),spam)", id(spam), spam)
+print("id(chess),chess", id(chess), chess)
--- 各种python书/Python编程快速上手/chap4/passingReference.py
+++ 各种python书/Python编程快速上手/chap4/passingReference.py
@@ -5,13 +5,17 @@
 @Created on: 2022/6/13 18:38
 """
 
+import copy
 
 def eggs(someParameter):
     print(id(someParameter))
-    someParameter.append('Hello')
+    copy.deepcopy(someParameter).append('Hello')
 
 
 spam = [1, 2, 3]
 print(id(spam))
 eggs(spam)
 print(spam)
+new_spam = copy.deepcopy(spam)
+print(id(new_spam))
+print(new_spam)
