47b30e3a774308ad9d43ec3ea904009655a7fa5a
sunliguo <sunliguo2006@qq.com>
2022-06-13 23:59:51 +0800

    手抄几个例子

--- 各种python书/Python编程从入门到实践/chap06/alien.py
+++ 各种python书/Python编程从入门到实践/chap06/alien.py
@@ -16,3 +16,5 @@ print(alien_0)
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
+
+del alien_0['points']
---
+++ 各种python书/Python编程从入门到实践/chap06/user.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+'''
+ @Time : 2022/6/13 23:57
+ @Author : sunliguo
+ @Email : sunliguo2006@qq.com
+ @File : user.py
+ @Project : github
+'''
+user_0 = {
+    'username':'efermi',
+    'first':'enrico',
+    'last':'fermi'
+}
+for key,value in user_0.items():
+    print(f"\nkey:{key}")
+    print(f"Value:{value}")
