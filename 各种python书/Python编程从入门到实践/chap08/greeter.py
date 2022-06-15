a60a511132aed57b89b04de54445db7579be8063
sunliguo <sunliguo2006@qq.com>
2022-06-15 20:09:54 +0800

    入门 chap8

---
+++ 各种python书/Python编程从入门到实践/chap08/greeter.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2022/1/16 8:20
+"""
+
+
+def greet_user(username):
+    """
+    显示简单的问候语
+    :return:
+    """
+    print(f'Hello!,{username.title()}')
+
+
+greet_user("sunliguo")
--- 各种python书/Python编程从入门到实践/chap08/pizza.py
+++ 各种python书/Python编程从入门到实践/chap08/pizza.py
@@ -4,16 +4,19 @@
 @contact: QQ376440229
 @Created on: 2022/1/16 8:02
 """
-def make_pizza(size,*toppings):
+
+
+def make_pizza(size, *toppings):
     """
     概述要制作的pizza
     :param size:
     :param toppings:
     :return:
     """
-    print(f'\nMaking a {size}-inch pizza with the follwing toppings:')
+    print(f'\nMaking a {size}-inch pizza with the following toppings:')
     for topping in toppings:
         print(f'-{topping}')
 
+
 make_pizza('pepperoni')
-make_pizza(12,'mushrooms','green peppers','extra cheese')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
