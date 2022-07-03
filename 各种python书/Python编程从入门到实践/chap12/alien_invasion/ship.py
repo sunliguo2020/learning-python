038a10af9bfdf8642886a59ccb12b3e1e03c295b
sunliguo <sunliguo2006@qq.com>
2022-07-03 10:49:35 +0800

    从入门到实践第13章

---
+++ 各种python书/Python编程从入门到实践/chap12/alien_invasion/ship.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2020/11/13 18:24
+"""
+import pygame
+
+
+class Ship:
+    """管理飞船的类"""
+
+    def __init__(self, ai_game):
+        """初始化飞船并设置其初始位置"""
+
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # 对于每艘新飞船，都将其放在屏幕底部的中央
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # 在飞船的属性中存储小数值
+        self.x = float(self.rect.x)
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据移动标志调整飞船的位置"""
+        # 更新飞船而不是rect对象的x值
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # 根据self.x更新rect对象
+        self.rect.x = self.x
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """让飞船在屏幕底端居中"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
