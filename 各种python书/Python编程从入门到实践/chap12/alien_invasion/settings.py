6b4a4adda62689d5cbc4f0d289508dabe6ef5176
sunliguo <sunliguo2006@qq.com>
2022-07-10 08:29:10 +0800

    外星人

--- 各种python书/Python编程从入门到实践/chap12/alien_invasion/settings.py
+++ 各种python书/Python编程从入门到实践/chap12/alien_invasion/settings.py
@@ -27,8 +27,8 @@ class Settings:
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
 
-        #外星人设置
+        # 外星人设置
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        #fleet_direction 为1 表示向右移，为-1表示向左移。
+        # fleet_direction 为1 表示向右移，为-1表示向左移。
         self.fleet_direction = 1
