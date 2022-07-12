fb000daa929b2eac49e0c37ed1c5c34f6051c950
sunliguo <sunliguo2006@qq.com>
2022-07-12 17:50:16 +0800

    删除加整理

--- python-test/class.py
+++ python-test/class.py
@@ -7,12 +7,18 @@
 
 @Created on: 2020/2/29 10:54
 """
+
+
 def Sayhi():
     print("say hello")
 
+
 Sayhi()
 
-def Calc(x,y,*ar):
-    print(x+y)
+
+def Calc(x, y, *ar):
+    print(x + y)
     print(ar)
-Calc(x=1,y=1,"sdf")
+
+
+Calc(x=1, y=1)
--- python-test/test354.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
- @Time : 2022/5/1 13:00
- @Author : sunliguo
- @Email : sunliguo2006@qq.com
- @File : test354.py
- @Project : pycharm
-'''
