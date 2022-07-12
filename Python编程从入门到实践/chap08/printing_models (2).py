7ebcb9efbfdbf9a81f24b9342603dad80bea00ed
sunliguo <sunliguo2006@qq.com>
2022-07-12 10:24:39 +0800

    移动培训文件

--- Python编程从入门到实践/chap08/printing_models (2).py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
- @Time : 2022/6/12 6:24
- @Author : sunliguo
- @Email : sunliguo2006@qq.com
- @File : printing_models.py
- @Project : learning-python
-'''
--- 各种python书/Python编程从入门到实践/chap08/pizza (2).py
+++ 各种python书/Python编程从入门到实践/chap08/pizza (2).py
@@ -6,14 +6,11 @@
  @File : pizza.py
  @Project : learning-python
 '''
-
-
 def make_pizza(*toppings):
     '''
     打印顾客点的所有配料。
     '''
     print(toppings)
 
-
 make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza('mushrooms','green peppers','extra cheese')
