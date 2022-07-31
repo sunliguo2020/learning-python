b7c37bb2ae9864c013b46763eac4983492b4691a
sunliguo <sunliguo2006@qq.com>
2022-07-31 08:17:28 +0800

    删除crawl库中lost表在别的表中有的数据

--- python-test/test.py
+++ python-test/test.py
@@ -7,8 +7,8 @@
 
 @Created on: 2019/11/23 14:22
 """
-for i in range(1,5):
-    for j in range(1,5):
-        for k in range(1,5):
-            if( i != k ) and (i != j) and (j != k):
-                print (i,j,k)
+for i in range(1, 5):
+    for j in range(1, 5):
+        for k in range(1, 5):
+            if (i != k) and (i != j) and (j != k):
+                print(i, j, k)
