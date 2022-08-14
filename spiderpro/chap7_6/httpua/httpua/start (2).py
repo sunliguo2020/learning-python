15e3cec85f3b549c6ab0f2ff5ae1076cf5878b57
sunliguo <sunliguo2006@qq.com>
2022-08-14 09:35:51 +0800

    thinkVision 20220814

---
+++ spiderpro/chap7_6/httpua/httpua/start (2).py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2020/11/16 12:55
+"""
+from scrapy import cmdline
+cmdline.execute('scrapy crawl httpuaspider'.split(' '))
---
+++ 各种python书/Python编程从入门到实践/chap08/printing_models (2).py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+ @Time : 2022/6/12 6:24
+ @Author : sunliguo
+ @Email : sunliguo2006@qq.com
+ @File : printing_models.py
+ @Project : learning-python
+'''
