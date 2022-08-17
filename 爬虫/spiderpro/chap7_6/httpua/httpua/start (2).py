3651504fe5934da740e442cdfbcdb943f36e2815
sunliguo <sunliguo2006@qq.com>
2022-08-18 07:56:53 +0800

    new s3 删除重复数据

--- 爬虫/spiderpro/chap7_6/httpua/httpua/start (2).py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@author: sunliguo
-@contact: QQ376440229
-@Created on: 2020/11/16 12:55
-"""
-from scrapy import cmdline
-cmdline.execute('scrapy crawl httpuaspider'.split(' '))
--- 爬虫/spiderpro/spider_test/baiduSpider/baiduSpider/start (2).py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@author: sunliguo
-@contact: QQ376440229
-@Created on: 2020/10/11 21:43
-"""
-from scrapy import cmdline
-cmdline.execute('scrapy crawl baidu'.split())
