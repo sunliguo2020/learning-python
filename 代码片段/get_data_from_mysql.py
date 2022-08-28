6dc314ca67d7cbd83a595a2e0a7e684696fbc982
sunliguo <sunliguo2006@qq.com>
2022-08-28 18:29:54 +0800

    手动处理冲突

--- 代码片段/get_data_from_mysql.py
+++ 代码片段/get_data_from_mysql.py
@@ -10,13 +10,11 @@ import math
 
 
 
-<<<<<<< HEAD
-def get_data_from_mysql(table=''):
-=======
+
 def loop_data_from_mysql(table='',sql = None):
     if sql is None:
         return -1
->>>>>>> 4d0ede0c3f44f1883275844924f649f10fe155bc
+
     conn = pymysql.Connect(host='192.168.1.207',
                            user='root',
                            password='admin',
@@ -32,11 +30,9 @@ def loop_data_from_mysql(table='',sql = None):
     # print(per)
 
     for i in range(0, per):
-<<<<<<< HEAD
+
         sql = f'select `id`,`idcard`,`personid` from `{table}` order by `mod_time` DESC limit {i * 5000},5000 ;'
-=======
-        # sql = f'select `id`,`idcard`,`personid` from `{table}` order by `mod_time` DESC limit {i * 5000},5000 ;'
->>>>>>> 4d0ede0c3f44f1883275844924f649f10fe155bc
+
         cur.execute(sql)
         inner_result = cur.fetchone()
         while inner_result:
