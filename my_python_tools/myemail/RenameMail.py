73afcbec4d014048cf73243f91949f926b077159
sunliguo <sunliguo2006@qq.com>
2022-09-23 21:26:48 +0800

    关于mail文件的处理，以前的没怎么注释，都忘记了。

--- my_python_tools/myemail/RenameMail.py
+++ my_python_tools/myemail/RenameMail.py
@@ -20,14 +20,18 @@ for (root, dirs, files) in os.walk(root_dir):
             count = count + 1
         mail_file_path = os.path.join(root, filename)
         md5sum = GetFileMd5(mail_file_path)
-        #文件md5获取失败，则继续
+        # 文件md5获取失败，则继续
         if md5sum is None:
             continue
         print(count, mail_file_path, md5sum)
 
         new_file_name = md5sum + '.eml'
         new_file_path = os.path.join(root_dir, new_file_name)
-        print(f'新的路径名：{new_file_path}')
+        # print(f'新的路径名：{new_file_path}')
 
         if not os.path.isfile(new_file_path):
-            os.renames(mail_file_path, new_file_path)
+            print('准备重命名文件')
+            try:
+                os.renames(mail_file_path, new_file_path)
+            except Exception as e:
+                print("重命名过程中出错：",e)
