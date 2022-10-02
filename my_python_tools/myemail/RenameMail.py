ddaeaddc0ae59c7a8a561d080fe02d9411554126
sunliguo <sunliguo2006@qq.com>
2022-10-02 08:19:25 +0800

    一个变量写错，导致修改名称后路径错误！

--- my_python_tools/myemail/RenameMail.py
+++ my_python_tools/myemail/RenameMail.py
@@ -11,27 +11,27 @@ from GetFileMd5 import GetFileMd5
 import os
 
 if __name__ == '__main__':
-    root_dir = r'F:\mail'
+    root_dir = r'F:\eml_new'
     count = 0
-for (root, dirs, files) in os.walk(root_dir):
+    for (root, dirs, files) in os.walk(root_dir):
 
-    for filename in files:
-        if filename.endswith('.eml'):
-            count = count + 1
-        mail_file_path = os.path.join(root, filename)
-        md5sum = GetFileMd5(mail_file_path)
-        # 文件md5获取失败，则继续
-        if md5sum is None:
-            continue
-        print(count, mail_file_path, md5sum)
+        for filename in files:
+            if filename.endswith('.eml'):
+                count = count + 1
+            mail_file_path = os.path.join(root, filename)
+            md5sum = GetFileMd5(mail_file_path)
+            # 文件md5获取失败，则继续
+            if md5sum is None:
+                continue
+            print(count, mail_file_path, md5sum)
 
-        new_file_name = md5sum + '.eml'
-        new_file_path = os.path.join(root_dir, new_file_name)
-        # print(f'新的路径名：{new_file_path}')
+            new_file_name = md5sum + '.eml'
+            new_file_path = os.path.join(root, new_file_name)
+            print(f'新的路径名：{new_file_path}')
 
-        if not os.path.isfile(new_file_path):
-            print('准备重命名文件')
-            try:
-                os.renames(mail_file_path, new_file_path)
-            except Exception as e:
-                print("重命名过程中出错：",e)
+            if not os.path.isfile(new_file_path):
+                print('准备重命名文件')
+                try:
+                    os.renames(mail_file_path, new_file_path)
+                except Exception as e:
+                    print("重命名过程中出错：",e)
