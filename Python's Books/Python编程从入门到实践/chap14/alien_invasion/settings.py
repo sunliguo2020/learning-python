c38e529002110c605198c65a7f0bb4b6c60601ca
sunliguo <sunliguo2006@qq.com>
2022-10-04 19:35:46 +0800

    14.2 提高等级 修改速度设置 重置速度

--- Python's Books/Python编程从入门到实践/chap14/alien_invasion/settings.py
+++ Python's Books/Python编程从入门到实践/chap14/alien_invasion/settings.py
@@ -30,5 +30,23 @@ class Settings:
         # 外星人设置
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        # fleet_direction 为1 表示向右移，为-1表示向左移。
+
+        #加快游戏节奏的速度
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_setting()
+
+    def initialize_dynamic_setting(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        #fleet_direction 为1 表示向右，为-1表示向左
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """提高速度设置"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
