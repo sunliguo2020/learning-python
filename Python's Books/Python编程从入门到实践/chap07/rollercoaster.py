bd4f99915af502f826d4ed65373914d298672fc3
sunliguo <sunliguo2006@qq.com>
2022-10-05 12:06:15 +0800

    chap07 的几个例子

---
+++ Python's Books/Python编程从入门到实践/chap07/rollercoaster.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time : 2022/10/5 11:13
+ @Author : sunliguo
+ @Email : sunliguo2006@qq.com
+ @File : rollercoaster.py
+ @Project : github
+"""
+height  = input("How tall are you,in inches?")
+height  = int(height)
+
+if height >= 48:
+    print("\nYou're taill enough to ride!")
+else:
+    print("\nYour're be able to ride when your're a little older.")
