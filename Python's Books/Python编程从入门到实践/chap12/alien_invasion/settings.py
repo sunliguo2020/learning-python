cca01e150c5406f711dbf4071519c8ca11285352
sunliguo <sunliguo2006@qq.com>
2022-10-05 17:59:02 +0800

    chap12 处理冲突

--- Python's Books/Python编程从入门到实践/chap12/alien_invasion/settings.py
+++ Python's Books/Python编程从入门到实践/chap12/alien_invasion/settings.py
@@ -18,39 +18,20 @@ class Settings:
 
         # 飞船设置
         self.ship_speed = 1.5
-<<<<<<< HEAD
+
         self.ship_limit = 3
 
         # 子弹设置
         self.bullet_speed = 1.5
-=======
-<<<<<<< HEAD
 
-        # 子弹设置
-        self.bullet_speed = 1.0
-=======
-        self.ship_limit = 3
-
-        # 子弹设置
-        self.bullet_speed = 1.5
->>>>>>> 3834090fde219e5410786a486c94f6cf2329ea24
->>>>>>> 4d0ede0c3f44f1883275844924f649f10fe155bc
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 4d0ede0c3f44f1883275844924f649f10fe155bc
+
 
         # 外星人设置
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
         # fleet_direction 为1 表示向右移，为-1表示向左移。
         self.fleet_direction = 1
-<<<<<<< HEAD
-=======
->>>>>>> 3834090fde219e5410786a486c94f6cf2329ea24
->>>>>>> 4d0ede0c3f44f1883275844924f649f10fe155bc
