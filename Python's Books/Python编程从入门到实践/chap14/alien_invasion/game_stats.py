e6088d9b9ad4b21029584a5cc3d6747adf34ce59
sunliguo <sunliguo2006@qq.com>
2022-10-06 10:50:15 +0800

    显示最高分和等级

--- Python's Books/Python编程从入门到实践/chap14/alien_invasion/game_stats.py
+++ Python's Books/Python编程从入门到实践/chap14/alien_invasion/game_stats.py
@@ -6,15 +6,24 @@
  @File : game_stats.py
  @Project : github
 '''
+
+
 class GameStats:
     """跟踪游戏的统计信息"""
-    def __init__(self,ai_game):
+
+    def __init__(self, ai_game):
         """初始化统计信息"""
         self.settings = ai_game.settings
         self.reset_stats()
 
-        #让游戏一开始处于非活动状态
+        # 让游戏一开始处于非活动状态
         self.game_active = False
+
+        # 任何情况下都不应重置最高得分
+        self.high_score = 0
+
     def reset_stats(self):
         """初始化在游戏运行期间可能变化的统计信息"""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
--- Python's Books/Python编程从入门到实践/chap14/alien_invasion/settings.py
+++ Python's Books/Python编程从入门到实践/chap14/alien_invasion/settings.py
@@ -31,8 +31,10 @@ class Settings:
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
 
-        #加快游戏节奏的速度
+        # 加快游戏节奏的速度
         self.speedup_scale = 1.1
+        # 外星人分数的提高速度
+        self.score_scale = 1.5
 
         self.initialize_dynamic_setting()
 
@@ -42,11 +44,17 @@ class Settings:
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
 
-        #fleet_direction 为1 表示向右，为-1表示向左
+        # fleet_direction 为1 表示向右，为-1表示向左
         self.fleet_direction = 1
 
+        # 计分
+        self.alien_points = 50
+
     def increase_speed(self):
-        """提高速度设置"""
+        """提高速度设置和外星人分数"""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        # print(self.alien_points)
