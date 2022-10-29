f709b999b79603bd91b85d13fdfe3e7e57a25a3e
sunliguo <sunliguo2006@qq.com>
2022-10-29 20:55:03 +0800

    以前写的，复制过来了。

---
+++ 代码片段/myshutil.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2022-10-28 17:08
+"""
+import shutil
+import os
+
+if __name__ == '__main__':
+    os.chdir('c:\\')
+    shutil.copy('c:\\spam.txt',"c:\\delicious")
