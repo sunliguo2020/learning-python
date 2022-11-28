6aae0e9eb9eb732f7cdbbc9d2ab3a19aae16fd40
sunliguo <sunliguo2006@qq.com>
2022-11-28 11:39:43 +0800

    整理python从入门到实践的例子

---
+++ Python's Books/Python编程从入门到实践/chap05/cars.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2022/11/28 11:30
+"""
+cars = ['audi','bmw','subaru','toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
--- Python's Books/Python编程从入门到实践/chap08/pizza (2).py
+++ Python's Books/Python编程从入门到实践/chap08/pizza (2).py
@@ -6,30 +6,19 @@
  @File : pizza.py
  @Project : learning-python
 '''
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 3834090fde219e5410786a486c94f6cf2329ea24
->>>>>>> 4d0ede0c3f44f1883275844924f649f10fe155bc
 def make_pizza(*toppings):
     '''
     打印顾客点的所有配料。
     '''
     print(toppings)
 
-<<<<<<< HEAD
+
 make_pizza('pepperoni')
 make_pizza('mushrooms','green peppers','extra cheese')
-=======
-<<<<<<< HEAD
+
 
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green peppers', 'extra cheese')
-=======
+
 make_pizza('pepperoni')
 make_pizza('mushrooms','green peppers','extra cheese')
->>>>>>> 3834090fde219e5410786a486c94f6cf2329ea24
->>>>>>> 4d0ede0c3f44f1883275844924f649f10fe155bc
