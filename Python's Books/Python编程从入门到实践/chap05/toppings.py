b9df4842add94f78a767fafde1646bc9e616edeb
sunliguo <sunliguo2006@qq.com>
2022-11-28 11:39:43 +0800

    整理python从入门到实践的例子

---
+++ Python's Books/Python编程从入门到实践/chap05/toppings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2022/11/28 11:32
+"""
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
