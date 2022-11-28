b919ca091a7aa298d20220600b81daac5a4a08e7
sunliguo <sunliguo2006@qq.com>
2022-11-28 11:53:13 +0800

    chap10的两个例子

--- Python's Books/Python编程从入门到实践/chap10/alice.py
+++ Python's Books/Python编程从入门到实践/chap10/alice.py
@@ -4,14 +4,14 @@
 @contact: QQ376440229
 @Created on: 2021/2/12 10:58
 """
-filename='alice.txt'
+filename = 'alice.txt'
 try:
-    with open(filename,encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         contents = f.read()
 except FileNotFoundError:
     print(f"Sorry,the file{filename} does not exist.")
 else:
-    #计算该文件大致包含多少个单词。
+    # 计算该文件大致包含多少个单词。
     words = contents.split()
     num_words = len(words)
     print(f"The file {filename} has about {num_words} words.")
