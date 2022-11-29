11f3b52654151401ad59a0b5f2ad3fd2b2e7c18d
sunliguo <sunliguo2006@qq.com>
2022-11-29 13:48:49 +0800

    chap18

--- Python's Books/Python编程从入门到实践/chap18/learning_logs/urls.py
+++ Python's Books/Python编程从入门到实践/chap18/learning_logs/urls.py
@@ -11,4 +11,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # 主页
     path('', views.index, name='index'),
+    #显示所有的主题
+    path('topics/',views.topics,name='topics')
 ]
--- Python's Books/Python编程从入门到实践/chap18/learning_logs/views.py
+++ Python's Books/Python编程从入门到实践/chap18/learning_logs/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import  Topic
 
 # Create your views here.
 
 def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
+def topics(request):
+    """显示所有的主题。"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topic':topics}
+    return render(request,'learning/topics.html',context)
