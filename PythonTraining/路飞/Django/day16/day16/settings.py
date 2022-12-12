9effa958560a8d4b6bcdcb8f7b18536efdaccb09
sunliguo <sunliguo2006@qq.com>
2022-12-12 23:07:43 +0800

    控制台添加sql日志

--- PythonTraining/路飞/Django/day16/day16/settings.py
+++ PythonTraining/路飞/Django/day16/day16/settings.py
@@ -128,6 +128,23 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        }
+    }
+}
