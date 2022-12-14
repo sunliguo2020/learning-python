c357314fb00b6a3668c79c22eb0ce2eca4b80863
sunliguo <sunliguo2006@qq.com>
2022-12-14 21:44:31 +0800

    chap06 用户注册

---
+++ Python's Books/Django+Vue.js 实战派Python Web开发与运维/chap06/myshop-test/app6/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
+
+# Create your views here.
+def user_reg(request):
+    if request.method == 'GET':
+        return render(request, '6/user_reg.html')
+    if request.method == 'POST':
+        uname = request.POST.get('username', '')
+        pwd = request.POST.get('password', '')
+        if User.objects.filter(username=uname):
+            info = '用户已经存在'
+        else:
+            d = dict(username=uname,
+                     password=pwd,
+                     email='111@111.com',
+                     is_staff=1,
+                     is_active=1,
+                     is_superuser=1)
+            user = User.objects.create_user(**d)
+            info = '注册成功，请登录'
+        return render(request, '6/user_reg.html', {'info': info})
