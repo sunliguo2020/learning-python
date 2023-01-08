58fe5dcf356397cc28982bfed558af8bd3a291a4
sunliguo <sunliguo2006@qq.com>
2023-01-08 14:54:52 +0800

    scrapy 豆瓣电影

---
+++ PythonTraining/千锋教育/spider2107/spider2107/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+# 爬虫获取到的数据需要组装成Item对象
+class MovieItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    rank = scrapy.Field()
+    subject = scrapy.Field()
---
+++ PythonTraining/千锋教育/spider2107/spider2107/spiders/douban.py
@@ -0,0 +1,20 @@
+import scrapy
+from scrapy import Selector
+
+from ..items import MovieItem
+
+
+class DoubanSpider(scrapy.Spider):
+    name = 'douban'
+    allowed_domains = ['movie.douban.com']
+    start_urls = ['https://movie.douban.com/top250']
+
+    def parse(self, response):
+        sel = Selector(response)
+        list_items = sel.css('#content >div>div.article>ol>li')
+        for list_item in list_items:
+            movie_item = MovieItem()
+            movie_item['title'] = list_item.css('span.title::text').extract_first()
+            movie_item['rank'] = list_item.css('span.rating_num::text').extract_first()
+            movie_item['subject'] = list_item.css('span.inq::text').extract_first()
+            yield movie_item
