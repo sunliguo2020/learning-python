1ba632227206a79ec2700350a1442a577021c765
sunliguo <sunliguo2006@qq.com>
2023-01-15 22:11:00 +0800

    跟老齐学python Django实战 chap01

---
+++ Python's Books/跟老齐学Python Django实战/chap01/mysite/blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2023-01-15 20:50
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.blog_title,name='blog_title'),
+    path('<int:article_id>/',views.blog_article,name='blog_detail'),
+]
--- 爬虫/scrapy/daglperson/daglperson/spiders/dagl.py
+++ 爬虫/scrapy/daglperson/daglperson/spiders/dagl.py
@@ -1,32 +1,28 @@
 import scrapy
-scrapy.http.response
+
 from scrapy import Request
-from scrapy.selector import Selector
 
 
 class DaglSpider(scrapy.Spider):
     name = 'dagl'
     allowed_domains = ['127.0.0.1:8000']
-    start_urls = ['http://127.0.0.1:8000/personid/list/']
+    start_urls = ['http://127.0.0.1:8000/personid/list/?page=123']
 
     def parse(self, response):
-        # sel = Selector(response)
-        # next_url = sel.xpath('//ul[@class="pagination"]/li[14]/a/@href')
-        # print(next_url)
-        # print(next_url.extract_first())
-        for i in range(1, 91477):
-            next_url = f'?page={i}'
-            next_url = response.urljoin(next_url)
-            if next_url:
-                yield Request(url=next_url, callback=self.content_parse, dont_filter=True)
 
-    def content_parse(self, response):
-        sel = Selector(response)
-        # list_items = sel.css('body > div:nth-child(2) > div > div.bs-example > div.panel.panel-default > table > tbody > tr:nth-child(1) > td:nth-child(3) > a')
-        list_items = sel.xpath('/html/body/div[1]/div/div[3]/div[1]/table/tbody/tr')
+        list_items = response.xpath('//tr/td[2]/a')
         for list_item in list_items:
-            url = list_item.xpath('./td[2]/a/@href')
-            # print(url.extract_first())
+            url = list_item.xpath('@href')
             url = response.urljoin(url.extract_first())
             print(url)
-            yield Request(url=url,callback=self.content_parse,dont_filter=True)
+            yield Request(url=url, callback=self.parse_none, dont_filter=True)
+
+        next_url = response.xpath('//ul[@class="pagination"]/li[last()-2]/a/@href').get()
+
+        next_url = response.urljoin(next_url)
+
+        if next_url:
+            yield Request(url=next_url, callback=self.parse, dont_filter=True)
+
+    def parse_none(self, response):
+        pass
