38f624847e38767acc3b7372159817879047345b
sunliguo <sunliguo2006@qq.com>
2023-01-19 22:06:04 +0800

    跟老齐学python Django实战 chap02 用户属性后台管理

--- Python's Books/跟老齐学Python Django实战/chap02/mysite/account/admin.py
+++ Python's Books/跟老齐学Python Django实战/chap02/mysite/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import UserProfile
+
+
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'birth', 'phone']
+    list_filter = ['phone', ]
