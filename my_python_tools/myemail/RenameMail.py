2f725378cd97b341f1196105a4ca46b6d328ced5
sunliguo <sunliguo2006@qq.com>
2023-03-02 20:54:06 +0800

    删除重复的代码，增加logging模块记录日志。

--- my_python_tools/myemail/RenameMail.py
+++ my_python_tools/myemail/RenameMail.py
@@ -7,9 +7,15 @@
  根据eml文件的MD5值改写文件名。
 """
 
-from GetFileMd5 import GetFileMd5
 import os
-from concurrent.futures import ThreadPoolExecutor
+import logging
+
+from GetFileMd5 import GetFileMd5
+
+logging.basicConfig(filename='rename_eml.log', level=logging.DEBUG,
+                    filemode='w',
+                    encoding='utf-8',
+                    format='%(asctime)s-%(filename)s[line:%(lineno)d]-%(message)s')
 
 
 def walk_dir(directory):
@@ -28,17 +34,18 @@ def walk_dir(directory):
             yield file_path
 
 
-def RenameFileName(mail_file_path=''):
+def rename_eml_file_name(mail_file_path=''):
     """
 
-    @param mail_file_path:
+    @param mail_file_path:要改名的eml文件
     @return:
     """
-    if mail_file_path.endswith('.eml'):
-
+    if os.path.isfile(mail_file_path) and mail_file_path.endswith('.eml'):
+        # 获取mail文件的md5值
         md5sum = GetFileMd5(mail_file_path)
         # 文件md5获取失败，则继续
         if md5sum is None:
+            logging.debug(f'获取{mail_file_path}的md5值失败!')
             return
         print(mail_file_path, md5sum)
 
@@ -51,31 +58,13 @@ def RenameFileName(mail_file_path=''):
             try:
                 os.renames(mail_file_path, new_file_path)
             except Exception as e:
-                print("重命名过程中出错：", e)
+                # print("重命名过程中出错：", e)
+                logging.debug(f"重命名{mail_file_path}失败,错误为{e}")
+        else:
+            logging.debug(f"{os.path.basename(mail_file_path)}已经存在该md5值的文件")
 
 
 if __name__ == '__main__':
-    root_dir = r'F:\mail'
-    count = 0
-for (root, dirs, files) in os.walk(root_dir):
-
-    for filename in files:
-        if filename.endswith('.eml'):
-            count = count + 1
-        mail_file_path = os.path.join(root, filename)
-        md5sum = GetFileMd5(mail_file_path)
-        # 文件md5获取失败，则继续
-        if md5sum is None:
-            continue
-        print(count, mail_file_path, md5sum)
-
-        new_file_name = md5sum + '.eml'
-        new_file_path = os.path.join(root_dir, new_file_name)
-        # print(f'新的路径名：{new_file_path}')
-
-        if not os.path.isfile(new_file_path):
-            print('准备重命名文件')
-            try:
-                os.renames(mail_file_path, new_file_path)
-            except Exception as e:
-                print("重命名过程中出错：", e)
+    root_dir = r'F:\eml'
+    for item in walk_dir(root_dir):
+        rename_eml_file_name(item)
