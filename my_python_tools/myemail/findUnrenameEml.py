53005abb7b6014cb4063762e50e40dce7ac2a8c7
sunliguo <sunliguo2006@qq.com>
2023-03-03 22:44:21 +0800

    检查是否有没有改名的文件！

---
+++ my_python_tools/myemail/findUnrenameEml.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time : 2023/3/3 21:44
+ @Author : sunliguo
+ @Email : sunliguo2006@qq.com
+ @File : findUnrenameEml.py
+ @Project : learning-python
+"""
+import os.path
+import re
+import shutil
+
+if __name__ == '__main__':
+    eml_path = r"f:\eml"
+    new_eml_path = r'f:\email'
+
+    for root, dirs, files in os.walk(eml_path, topdown=False):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            if not filename.endswith('.eml'):
+                continue
+            file_name = filename.replace('.eml', '')
+            if not re.findall(r"([a-fA-F\d]{32})", file_name) or len(file_name) != 32:
+                print(f'准备移动{filename}')
+                shutil.move(file_path, new_eml_path)
+            else:
+                print(f"{filename}已经改过名了！")
