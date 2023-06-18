d8f6d6afaf6b689a75a8fb22ae74028798742182
sunliguo <sunliguo2006@qq.com>
2023-06-18 17:36:08 +0800

    消息队列

---
+++ PythonTraining/小飞有点东西/消息队列/q.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2023-06-18 15:07
+"""
+import queue
+
+q = queue.Queue()
+print(type(q))
+
+p = queue.Queue
+print(type(p))
