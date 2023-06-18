a7c3d89069f1f69c5d78a63fdbd66963bdd3bfcb
sunliguo <sunliguo2006@qq.com>
2023-06-18 17:35:38 +0800

    gui学习

--- PythonTraining/马哥/gui/demo1.py
+++ PythonTraining/马哥/gui/demo1.py
@@ -7,13 +7,16 @@ root.title('我的第一个GUI程序')
 
 root.geometry("500x300+100+200")
 
-btn01  = Button(root)
+btn01 = Button(root)
 btn01['text'] = "点我就送花"
 btn01.pack()
+
+
 def songhua(e):
-    messagebox.showinfo('Message',"送你一朵玫瑰花")
+    messagebox.showinfo('Message', "送你一朵玫瑰花")
     print("送你99朵玫瑰花")
 
-btn01.bind('<Button-1>',songhua)
+
+btn01.bind('<Button-1>', songhua)
 
 root.mainloop()
