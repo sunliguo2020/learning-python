55a9af05c18ae47398a4f93054af30982b8753e5
sunliguo <sunliguo2006@qq.com>
2023-06-18 17:35:02 +0800

    format

--- PythonTraining/基础-实操-项目源码/项目02 Python实战操作源码/Code（实例源码及使用说明）/05/15/videoTest.py
+++ PythonTraining/基础-实操-项目源码/项目02 Python实战操作源码/Code（实例源码及使用说明）/05/15/videoTest.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 # 文件名称   ：videoTest.PY
 # 开发工具   ：PyCharm
-'''
+"""
   Python控制摄像头拍照
-'''
-import cv2 # 导入cv2模块
-cap = cv2.VideoCapture(0) # 创建视频对象
-while(1):
-    ret, frame = cap.read() # 显示视频
-    cv2.imshow("Capture", frame) # 显示视频窗口
+"""
+import cv2  # 导入cv2模块
+
+cap = cv2.VideoCapture(0)  # 创建视频对象
+while 1:
+    ret, frame = cap.read()  # 显示视频
+    cv2.imshow("Capture", frame)  # 显示视频窗口
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.imwrite("test.jpeg", frame) # 保存图片
+        cv2.imwrite("test.jpeg", frame)  # 保存图片
         break
-cap.release() # 释放占用资源
-cv2.destroyAllWindows() # 关闭窗体
+cap.release()  # 释放占用资源
+cv2.destroyAllWindows()  # 关闭窗体
