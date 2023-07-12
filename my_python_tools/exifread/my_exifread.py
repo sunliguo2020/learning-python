68f803759129bac6d7cf4965c123cb2466441ec9
sunliguo <sunliguo2006@qq.com>
2023-07-12 21:25:35 +0800

    改进

--- my_python_tools/exifread/my_exifread.py
+++ my_python_tools/exifread/my_exifread.py
@@ -1,6 +1,9 @@
 import exifread
 import os
 import shutil
+import logging
+
+logger = logging.getLogger()
 
 '''
 {'MI 5s', 'HUAWEI G730-U00', 'ARS-AL00'}
@@ -9,6 +12,12 @@ import shutil
 
 
 def mymovefile(srcfile, dstfile):
+    """
+    移动文件
+    @param srcfile:
+    @param dstfile:
+    @return:
+    """
     if not os.path.isfile(srcfile):
         print("{0} is not exits!".format(srcfile))
     else:
@@ -20,8 +29,15 @@ def mymovefile(srcfile, dstfile):
 
 
 def mycopyfile(srcfile, dstfile):
-    if not os.path.isfile(srcfile):
-        print("{0} is not exits!".format(srcfile))
+    """
+    拷贝源文件到目标目录中
+    @param srcfile:  源文件
+    @param dstfile:     目的文件
+    @return:
+    """
+    if not os.path.isfile(srcfile) or os.path.isfile(dstfile):
+        print("{0} is not exits!".format(srcfile),end='')
+        print(f"目标文件{dstfile}已经存在！")
     else:
         fpath, fname = os.path.split(dstfile)
         if not os.path.exists(fpath):
@@ -33,17 +49,16 @@ def mycopyfile(srcfile, dstfile):
 def exif_Image_Model(file_name):
     """
     返回 jpg文件中exif的 设备型号(Image Model)
-    :param file_name:
+    :param file_name: jpg文件
     :return:
     """
-    f = open(file_name, 'rb')
-    try:
-        tag = exifread.process_file(f)
-    except Exception as  e:
-        print(file_name)
-    else:
-        tag = {}
-    f.close()
+    with open(file_name,'rb') as fp:
+        try:
+            tag = exifread.process_file(fp)
+        except Exception as e:
+            logger.error(f"读取jpg文件获取Image Model中出错,{e}")
+        else:
+            tag = {}
 
     if tag.get("Image Model") is not None:
         image_camera = str(tag.get("Image Model")).strip()
@@ -68,7 +83,6 @@ def get_img_model(img_dir):
             if os.path.splitext(file_name)[-1][1:] == "jpg":
                 file_path_name = os.path.join(root, file_name)
 
-                print(file_path_name)
                 file_image_model = exif_Image_Model(file_path_name)
 
                 if file_image_model is not None:
@@ -78,7 +92,8 @@ def get_img_model(img_dir):
 
 
 if __name__ == "__main__":
-    img_camera_dict = get_img_model(r'U:\手机\来自-手机HUAWEI G730-U00')
+    img_camera_dict = get_img_model(r'.')
+    print(f'img_camera_dict:{img_camera_dict}')
     print(set(img_camera_dict.values()))
     print(len(img_camera_dict))
 
@@ -87,7 +102,3 @@ if __name__ == "__main__":
         file_name = os.path.split(file_path)[-1]
         if camera != "HUAWEI G730-U00":
             print(file_path, camera)
-            # mymovefile(file_path,os.path.join("h:\\",camera,file_name))
-
-        # print("file_path:{0},camear:{1}".format(file_path,camera))
-        # mymovefile(file_path,os.path.join("h:\\",camera,file_name))
