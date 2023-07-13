9086fd3644d7a531d7b4e3aa00444c250697ea86
sunliguo <sunliguo2006@qq.com>
2023-07-13 13:23:37 +0800

    小飞学的例子

--- PythonTraining/小飞有点东西/python零基础到全栈/28-模块.py
+++ PythonTraining/小飞有点东西/python零基础到全栈/28-模块.py
@@ -4,12 +4,27 @@
 @contact: QQ376440229
 @Created on: 2023-06-28 12:52
 """
-import module
-
+# import module
+# from module import func1
+# from module import func2
+# from module import get
 # print(module.name)
 # print(module.__name__)
 # print(dir(module))
 # print(module.__file__)
 # module.func1()
-name = '礼包'
-module.get()
+# name = '礼包'
+# module.get()
+# from module import name
+#
+# print(name)
+# get()
+# func2()
+# get()
+# print(name)
+#
+# print(module.__all__)
+
+from module import *
+print(name)
+get()
