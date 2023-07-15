2e3162230e8306a488b039024d99b23036aad9f8
sunliguo <sunliguo2006@qq.com>
2023-07-15 13:33:58 +0800

    获取图片的url

--- Django/web02_shop/web02_shop/urls.py
+++ Django/web02_shop/web02_shop/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from users.views import FileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('users.urls'))
+    path('api/users/', include('users.urls')),
+    re_path(r'file/image/(.+?)/', FileView.as_view()),
 ]
