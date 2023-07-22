ec203c41192736131c10bcde43c1df6a04acd1ee
sunliguo <sunliguo2006@qq.com>
2023-07-22 21:24:10 +0800

    最终方案

--- Django/DRFdemo1/app1/urls.py
+++ Django/DRFdemo1/app1/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('users/', views.user_list),
-    re_path(r'users/(.+?)/', views.user_detail),
-    path('book/', views.BookView.as_view()),
-    path('book2/', views.BookView2.as_view()),
+    # path('users/', views.user_list),
+
+    # re_path(r'users/(.+?)/', views.user_detail),
+    # path('users/<int:id>/', views.user_detail),
+
+    path('users/', views.UserView.as_view({"get": 'list', 'post': 'create'})),
+    path('users/<int:id>/', views.UserView.as_view({'get': 'retrieve',
+                                                    'put': 'update',
+                                                    'DELETE': "destroy"})),
+
 ]
--- Django/DRFdemo1/app1/views.py
+++ Django/DRFdemo1/app1/views.py
@@ -1,9 +1,4 @@
-from django.http import HttpResponse, Http404
-from django.views import View
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
+from rest_framework.viewsets import ModelViewSet
 
 from .models import UserInfo
 from .serializers import UserInfoSerializer
@@ -11,81 +6,11 @@ from .serializers import UserInfoSerializer
 
 # Create your views here.
 
-class UserListView(APIView):
+class UserView(ModelViewSet):
     # 设置模型类
-    model = UserInfo
-    serializer = UserInfoSerializer
-
-    def get(self, request, format=None):
-        # 获取用户信息列表
-        users = self.model.objects.all()
-        # 创建序列化对象
-        ser = self.serializer(users, many=True)
-        result = {
-            'data': ser.data,
-            'code': 200,
-            "message": "OK"
-        }
-        return Response(result, status=status.HTTP_200_OK)
-
-    def post(self, request, format=None):
-        # 创建序列化器
-        print(request.data)
-        ser = self.serializer(data=request.data)
-        # 校验请求数据
-        if ser.is_valid():
-            # 校验通过，则添加数据到数据
-            ser.save()
-            return Response({'code': 201, 'data': ser.data, "message": 'OK'})
-        else:
-            return Response({'code': 400, 'message': ser.errors}, status=status.HTTP_400_BAD_REQUEST)
+    queryset = UserInfo.objects.all()
+    # 设置序列化器类
+    serializer_class = UserInfoSerializer
+    lookup_field = 'id'
 
 
-class UserDetailView(APIView):
-    # 设置模型类
-    model = UserInfo
-    serializer = UserInfoSerializer
-
-    def get_object(self, id):
-        try:
-            return self.model.objects.get(id=id)
-        except Exception as e:
-            raise Http404()
-
-    def get(self, request, id, format=None):
-        obj = self.get_object(id)
-        ser = self.serializer(obj)
-        return Response({'code': 200, "data": ser.data, 'message': "OK"}, status=status.HTTP_200_OK)
-
-    def put(self, request, id, format=None):
-        obj = self.get_object(id)
-        # 修改单个用户资源
-        ser = self.serializer(instance=obj, data=request.data)
-        if ser.is_valid():
-            ser.save()
-            return Response({'code': 200, "data": ser.validated_data, "message": "OK"}, status=status.HTTP_200_OK)
-        else:
-            return Response({'code': 400, 'message': ser.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id, format=None):
-        obj = self.get_object(id)
-        obj.delete()
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
-
-
-class BookView(View):
-    def get(self, request):
-        return HttpResponse('BookView get run')
-
-    def post(self, request):
-        return HttpResponse('BookView post run')
-
-    def delete(self, request):
-        return HttpResponse('BookView delete run')
-
-
-class BookView2(APIView):
-    def post(self, request):
-        print(type(request.data))
-        print(request.data)
-        return HttpResponse(f"request.data{request.data}")
