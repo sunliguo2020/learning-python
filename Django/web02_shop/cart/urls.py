09561efea49de4d608e8cc2df3c36bf903f58cf5
sunliguo <sunliguo2006@qq.com>
2023-08-03 07:54:36 +0800

    购物车URL

--- Django/web02_shop/cart/urls.py
+++ Django/web02_shop/cart/urls.py
@@ -3,5 +3,17 @@ from django.urls import path
 from cart import views
 
 urlpatterns = [
-    path('goods/', views.CartView.as_view({'get': 'list', 'post': 'create'}))
+    # 添加商品到购物车和获取购物车商品列表
+    path('goods/', views.CartView.as_view({'get': 'list',
+                                           'post': 'create'})),
+    # 修改购物车商品的数量
+    path('goods/<int:pk>/', views.CartView.as_view({
+        "put": 'update_goods_number',
+        "delete": "destroy"
+    })),
+    # 修改商品的选中状态
+    path('goods/<int:pk>/checked/', views.CartView.as_view({
+        "put": "update_goods_status"
+    })),
+
 ]
