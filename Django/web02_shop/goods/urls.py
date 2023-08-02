674585159cff36d20195bdb7fd2dde3b85b6cbdd
sunliguo <sunliguo2006@qq.com>
2023-08-03 07:55:56 +0800

    格式化代码

--- Django/web02_shop/goods/urls.py
+++ Django/web02_shop/goods/urls.py
@@ -5,16 +5,11 @@ from goods import views
 urlpatterns = [
     # 商品首页获取
     path('index/', views.Index.as_view()),
+
     # 商品列表接口
-    path('goods/', views.GoodsView.as_view({
-        "get": "list",
-
-    })),
+    path('goods/', views.GoodsView.as_view({"get": "list"})),
     # 获取单个商品的接口
-    path('goods/<int:pk>/', views.GoodsView.as_view({
-        "get": "retrieve",
-
-    })),
+    path('goods/<int:pk>/', views.GoodsView.as_view({"get": "retrieve", })),
 
     # 收藏商品和获取收藏列表
     path("goods/collect/", views.CollectViewSet.as_view({
@@ -23,9 +18,6 @@ urlpatterns = [
     })),
 
     # 取消收藏商品
-    path("goods/collect/<int:pk>/", views.CollectViewSet.as_view({
-        "delete": "destroy",
-
-    }))
+    path("goods/collect/<int:pk>/", views.CollectViewSet.as_view({"delete": "destroy"}))
 
 ]
