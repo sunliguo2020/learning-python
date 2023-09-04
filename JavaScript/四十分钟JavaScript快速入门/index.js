a9e47e37e79bc1eb2bedea521ad4c73e97b0f1e5
sunliguo <sunliguo2006@qq.com>
2023-09-04 21:27:01 +0800

    JavaScript学习

---
+++ JavaScript/四十分钟JavaScript快速入门/index.js
@@ -0,0 +1,20 @@
+//这是我的第一个Js代码
+console.log('Hello Wold');
+
+//String Number Boolean null undefined
+const username = "John"
+const age = 30
+const rate = 4.5
+const isCool = true
+const x = null;
+const y = undefined;
+let z
+
+console.log(typeof x)
+
+window.onload = function(){
+    //基本类型 string number boolean undefined null
+    //复杂类型/引用类型 array object function
+    // 一切皆对象
+    //解释型 编译型
+}
