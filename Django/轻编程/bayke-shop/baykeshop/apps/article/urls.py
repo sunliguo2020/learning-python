1c9e45a83269c7eb7ba31f87625e482f2501e91c
sunliguo <sunliguo2006@qq.com>
2023-11-09 23:05:43 +0800

    商城文章listView 和DetailView

--- Django/轻编程/bayke-shop/baykeshop/apps/article/urls.py
+++ Django/轻编程/bayke-shop/baykeshop/apps/article/urls.py
@@ -7,4 +7,6 @@ app_name = 'article'
 urlpatterns = [
     path('def_index/', views.index,name='index'),
     path('cls_index/', views.IndexView.as_view(),name='index2'),
+    path('listview/', views.BaykeArticleListView.as_view(),name='listview'),
+    path('detailview/<int:pk>/', views.BaykeArticleDetailView.as_view(),name='detailview'),
 ]
--- Django/轻编程/bayke-shop/baykeshop/apps/article/views.py
+++ Django/轻编程/bayke-shop/baykeshop/apps/article/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import View
+from django.views.generic import View,ListView,DetailView
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,3 +11,14 @@ class IndexView(View):
         return render(request,'article/index.html')
     def post(self,request,*args, **kwargs):
         return render(request)
+    
+class BaykeArticleListView(ListView):
+    template_name = 'article/list.html'
+    context_object_name = 'article_list'
+    queryset = [i for i in range(1,20)]
+
+
+class BaykeArticleDetailView(DetailView):
+    template_name = 'article/detail.html'
+    context_object_name = 'article'
+    queryset = []
