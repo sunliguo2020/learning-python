85256665098179fc3835e23b0bf6ad68003dceaf
sunliguo <sunliguo2006@qq.com>
2023-11-09 20:28:43 +0800

    侧边栏归档页

--- Django/轻编程/MyBlog/blog/urls.py
+++ Django/轻编程/MyBlog/blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('category/<int:category_id>/', views.category_list, name='category_list'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('search/', views.search, name='search'),
+    path('archives/<int:year>/<int:month>/', views.archives, name='archives'),
 ]
