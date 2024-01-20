b7635fb5b37ea9782301182b06e76f1d70fd56c6
sunliguo <sunliguo2006@qq.com>
2024-01-20 22:52:17 +0800

    基本实现（需要动态获取数据裁剪才可以）

--- 爬虫/案例/288job/滑动登录.py
+++ 爬虫/案例/288job/滑动登录.py
@@ -6,9 +6,11 @@
  @File : 滑动登录.py
  @Project : learning-python
 """
+import io
 import re
 import time
 
+from PIL import Image
 import ddddocr
 import requests
 from selenium import webdriver
@@ -54,6 +56,7 @@ def fetch_bg_func(dv):
         By.ID, 'slideBg'
     )
     style_string = tag_obj.get_attribute('style')
+    # 获取背景图片大小
     match_list = re.findall('url\(\"(.*)\"\);', style_string)
     if match_list:
         return match_list[0]
@@ -68,8 +71,10 @@ print('背景图', bg_image_url)
 def fetch_slice_func(dv):
     tag_obj = driver.find_element(
         By.XPATH,
-        '//*[@id="tcOperation"]/div[7]'
+        '//*[@id="tcOperation"]/div[8]'
     )
+    print('缺口图截图')
+    tag_obj.screenshot('slice_screenshot.png')
     style_string = tag_obj.get_attribute('style')
     match_list = re.findall('url\(\"(.*)\"\);', style_string)
     if match_list:
@@ -81,22 +86,44 @@ slice_image_url = WebDriverWait(driver, 30, 0.5).until(fetch_slice_func)
 print("缺口图", slice_image_url)
 
 # 识别图片
+
+# 获取图片
 slice_bytes = requests.get(slice_image_url).content
 bg_bytes = requests.get(bg_image_url).content
 
+# 调整背景图片大小
+image = Image.open(io.BytesIO(bg_bytes))
+# 调整图像大小为宽度为340 像素，高度为243像素
+resized_image = image.resize((340, 243))
+
+bg_bytes = io.BytesIO()
+resized_image.save(bg_bytes, 'png')
+# bg_bytes = bg_bytes.read()
+resized_image.save('bg.png')
+
+# 保存原始缺口图片
 with open('slice.png', 'wb') as fp:
     fp.write(slice_bytes)
-with open('bg.png', 'wb') as fp:
-    fp.write(bg_bytes)
 
-from PIL import Image
+# with open('bg.png', 'wb') as fp:
+#     fp.write(bg_bytes)
 
+# 裁剪得到缺口图
 img = Image.open('slice.png')
 print(img.size)
+
+# 裁剪图片需要优化
 cropped = img.crop((150, 490, 240, 588))  # (left, upper, right, lower)
+# 缩放图片
+cropped = cropped.resize((60, 60))
 cropped.save("slice2.png")
+
 with open('slice2.png', 'rb') as fp:
     slice_bytes = fp.read()
+
+with open('bg.png', 'rb') as fp:
+    bg_bytes = fp.read()
+
 # 使用ddddocr识别
 slide = ddddocr.DdddOcr(det=False, ocr=False, show_ad=False)
 res = slide.slide_match(slice_bytes, bg_bytes, simple_target=True)
@@ -108,11 +135,12 @@ print('滑动距离', x1)
 # 滑动按钮
 # driver.switch_to.default_content()
 btn_tag = driver.find_element(By.XPATH, '//*[@id="tcOperation"]/div[6]')
-print(btn_tag)
+# print(btn_tag)
 
 ActionChains(driver).click_and_hold(btn_tag).perform()
 ActionChains(driver).move_by_offset(xoffset=x1, yoffset=0).perform()
 ActionChains(driver).release().perform()
 
 print('移动完成')
+
 time.sleep(500)
