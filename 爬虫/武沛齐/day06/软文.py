a0e3652e6c077cb045b1f63a20c2268daf2f9b4e
sunliguo <sunliguo2006@qq.com>
2024-01-22 20:26:33 +0800

    验证码登录 二次整理

--- 爬虫/武沛齐/day06/软文.py
+++ 爬虫/武沛齐/day06/软文.py
@@ -20,21 +20,14 @@ with open('xx.png', 'wb') as fp:
 
 ocr = ddddocr.DdddOcr(show_ad=False)
 code = ocr.classification(res.content)
-print(code)
+print(f"验证码：{code}")
 
-cookies = {
-    'utm_fv': 'https://i.ruanwen.la/',
-    'pid': 'eyJpdiI6IkZCOXhiNE9mSE55N09HWFwvUTZCU1dRPT0iLCJ2YWx1ZSI6IlpmVTBkam5SRHh5VVcwbERDOXV2R1E9PSIsIm1hYyI6ImQ0M2VmYzhmMDBmZmQ2YzRmNzJkYWY5ZWViM2JmNDVhYThkZWRiZTViZDQ4ZjgyMmJkZmQ2YzZhMjAzOTMxZmMifQ%3D%3D',
-    'XSRF-TOKEN': 'eyJpdiI6IlRKNmF3K1h4bm1yV3pNVEFyd25pa3c9PSIsInZhbHVlIjoia21PSnJ3WUZyTzREVE5wSE5ZTWVvXC9IXC9jc3FKbDFGUXpTZjhseTFqVlQ2RzBaY1wvenNEM3FPZVdtU1wveWhkNU8iLCJtYWMiOiI2YzAwZDBkN2EzNjdiZGRkNTgxZDIyOTRkNmQ2N2U3YjI1OGJlMDUzMDM0YTU4NmZmZGU5MjdiZTMxMDBlZWJmIn0%3D',
-    'SESSION_COOKIE': 'eyJpdiI6Imk5ZFNaZnVKN05BVHgySmdlRkpFSEE9PSIsInZhbHVlIjoiZTVDZ2NUY3dlMllpWnI3RDFQNFRtRCsxc1dyVHg3aUJvXC9SODY3Y2hzWkZQREVLNmNBUU81WDVCXC9Jdk1POEVwIiwibWFjIjoiMDcyMTljY2RkNDJmMzllZWExZGFhOTQzNjBmNzQ1MmVhZTRiN2QzZWVmOTRjYmM2YWM0MTdkYzZhYmVhZWU3YSJ9',
-}
 
 headers = {
     'authority': 'api.ruanwen.la',
     'accept': '*/*',
     'accept-language': 'zh-CN,zh;q=0.9',
     'content-type': 'application/json;charset=UTF-8',
-    # 'cookie': 'utm_fv=https://i.ruanwen.la/; pid=eyJpdiI6IkZCOXhiNE9mSE55N09HWFwvUTZCU1dRPT0iLCJ2YWx1ZSI6IlpmVTBkam5SRHh5VVcwbERDOXV2R1E9PSIsIm1hYyI6ImQ0M2VmYzhmMDBmZmQ2YzRmNzJkYWY5ZWViM2JmNDVhYThkZWRiZTViZDQ4ZjgyMmJkZmQ2YzZhMjAzOTMxZmMifQ%3D%3D; XSRF-TOKEN=eyJpdiI6IlRKNmF3K1h4bm1yV3pNVEFyd25pa3c9PSIsInZhbHVlIjoia21PSnJ3WUZyTzREVE5wSE5ZTWVvXC9IXC9jc3FKbDFGUXpTZjhseTFqVlQ2RzBaY1wvenNEM3FPZVdtU1wveWhkNU8iLCJtYWMiOiI2YzAwZDBkN2EzNjdiZGRkNTgxZDIyOTRkNmQ2N2U3YjI1OGJlMDUzMDM0YTU4NmZmZGU5MjdiZTMxMDBlZWJmIn0%3D; SESSION_COOKIE=eyJpdiI6Imk5ZFNaZnVKN05BVHgySmdlRkpFSEE9PSIsInZhbHVlIjoiZTVDZ2NUY3dlMllpWnI3RDFQNFRtRCsxc1dyVHg3aUJvXC9SODY3Y2hzWkZQREVLNmNBUU81WDVCXC9Jdk1POEVwIiwibWFjIjoiMDcyMTljY2RkNDJmMzllZWExZGFhOTQzNjBmNzQ1MmVhZTRiN2QzZWVmOTRjYmM2YWM0MTdkYzZhYmVhZWU3YSJ9',
     'origin': 'https://i.ruanwen.la',
     'platform': 'xinmeibao',
     'referer': 'https://i.ruanwen.la/',
@@ -57,7 +50,9 @@ json_data = {
     'identity': 'advertiser',
 }
 
-response = requests.post('https://api.ruanwen.la/api/auth/authenticate', cookies=cookies, headers=headers,
+response = requests.post('https://api.ruanwen.la/api/auth/authenticate',
+                         # cookies=cookies,
+                         # headers=headers,
                          json=json_data)
 
 print(response.json())
