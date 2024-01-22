275065f9ba2a8fbae60c95e6a42803b72df50e07
sunliguo <sunliguo2006@qq.com>
2024-01-22 20:09:11 +0800

    北大未名登录  二次整理

--- 爬虫/武沛齐/day05/北大.py
+++ 爬虫/武沛齐/day05/北大.py
@@ -3,20 +3,23 @@
 @author: sunliguo
 @contact: QQ376440229
 @Created on: 2024-01-13 18:00
+北大未名论坛登录
 """
 import hashlib
 import time
 
 import requests
 
+# 1、首页
 res = requests.get('https://bbs.pku.edu.cn/v2/home.php')
 cookie_dict = res.cookies.get_dict()
-print(cookie_dict)
+print(f"第一次登录，返回cookie:{cookie_dict}")
 
 # 2、登录
 user = "zhangkai"
 pwd = '123123'
 ctime = int(time.time())
+
 data_string = f"{pwd}{user}{ctime}{pwd}"
 obj = hashlib.md5()
 
