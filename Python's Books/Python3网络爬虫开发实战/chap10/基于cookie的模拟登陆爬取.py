cd6a655b87910d77dd51d9e65996045cc1048d85
sunliguo <sunliguo2006@qq.com>
2024-02-26 20:24:21 +0800

    python 模拟登录

---
+++ Python's Books/Python3网络爬虫开发实战/chap10/基于cookie的模拟登陆爬取.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time : 2024/2/21 12:13
+ @Author : sunliguo
+ @Email : sunliguo2006@qq.com
+"""
+import requests
+from urllib.parse import urljoin
+
+BASE_URL = 'https://login2.scrape.center/'
+LOGIN_URL = urljoin(BASE_URL, '/login')
+INDEX_URL = urljoin(BASE_URL, '/page/1')
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+response_login = requests.post(LOGIN_URL, data={
+    'username': USERNAME,
+    'password': PASSWORD
+}, allow_redirects=False)
+
+print(response_login.text)
+cookies = response_login.cookies
+print("cookies", cookies)
+
+response_index = requests.get(INDEX_URL, cookies=cookies)
+
+print('Response Status', response_index.status_code)
+print('Response Url', response_index.url)
