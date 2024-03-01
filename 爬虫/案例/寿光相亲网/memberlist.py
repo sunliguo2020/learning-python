ddf9d4c42237c3d9a57244f2657eca7ab1651570
sunliguo <sunliguo2006@qq.com>
2024-03-02 07:44:01 +0800

    爬虫案例更新

--- 爬虫/案例/寿光相亲网/memberlist.py
+++ 爬虫/案例/寿光相亲网/memberlist.py
@@ -30,6 +30,10 @@ headers = {
 
 
 def get_member_list(page):
+    """
+    获取某一页的会员列表
+
+    """
     params = {
         'actiontype': 'member',
         'page': page,
@@ -38,7 +42,6 @@ def get_member_list(page):
     member_list_url = 'https://www.sgjhw.com/pc/love/listrecommend'
     response = requests.get(member_list_url, params=params, headers=headers)
 
-    # print(response.json())
     return response
 
 
@@ -47,8 +50,6 @@ if __name__ == '__main__':
     for page in range(1, 213):
         print(page)
         resp = get_member_list(page)
-        # resp.encoding = 'utf-8'
-        # print(json.dumps(resp.json()))
 
         resp_json = resp.json().get('data').get('list')
         member_list.extend(resp_json)
@@ -56,12 +57,14 @@ if __name__ == '__main__':
         # 加上参数ensure_ascii 避免中文显示为ascii
         json_str = json.dumps(resp_json, ensure_ascii=False)
 
-        with open(f"./json/{page}.json", mode='w', encoding='utf-8') as fp:
-            # fp.write(resp.content.decode("unicode-escape"))
-            # json.dump(obj=resp.json(), fp=fp)
-            fp.write(json_str)
+        # 单独保存一页的数据
+        # with open(f"./json/{page}.json", mode='w', encoding='utf-8') as fp:
+        #     # fp.write(resp.content.decode("unicode-escape"))
+        #     # json.dump(obj=resp.json(), fp=fp)
+        #     fp.write(json_str)
 
-    with open(f'member_list.csv', 'w', newline='', encoding='utf-8') as f2:
+    #  写入csv
+    with open(r'd:\member_list.csv', 'w', newline='', encoding='utf-8') as f2:
         csv_writer = csv.DictWriter(f2, fieldnames=resp_json[0].keys())
         csv_writer.writeheader()
         for item in member_list:
