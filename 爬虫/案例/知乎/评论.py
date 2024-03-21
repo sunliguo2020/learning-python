3f29e9f344a02d50a314872fe15b8b6c10af772c
sunliguo <sunliguo2006@qq.com>
2024-03-22 07:30:51 +0800

    知乎 获取评论更新

--- 爬虫/案例/知乎/评论.py
+++ 爬虫/案例/知乎/评论.py
@@ -4,48 +4,59 @@
 @contact: QQ376440229
 @Created on: 2024-03-19 13:16
 """
-import time
-
+import execjs
 import fake_useragent
 import requests
 
 ua = fake_useragent.UserAgent()
+
+
+def get_x96(tt, tu):
+    with open('demo5.js', encoding='utf-8') as fp:
+        js_content = fp.read()
+
+    jj = execjs.compile(js_content)
+    return jj.call('x93', tt, tu)
+
+
+session = requests.session()
+
 cookies = {
-    '_zap': '6cc272a9-fab9-48cd-80bf-abff70c21536',
-    'd_c0': 'AHBWO4qj1xePTmvvvLzm1-P5ya-H2eLVjLI=|1702388268',
-    '_xsrf': 'ilBJqmFgU8UurgfMhq65J4dEu0tegi1I',
-    '__snaker__id': '61tnc5jXnp6uo3h0',
-    'Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49': '1710820695',
-    'SESSIONID': 'Y7j6W5CKQIl8e6KEKLplDKox4rqymjIQm4qC2qxojuV',
-    'JOID': 'VVgVC0xr-EsCjH9mOGQ_m5I18JUoNc8pO9tNJXs6ticy5kkqfe3itWiLeG4_4fTDVbcN2_6CONFj-CWzob_t-5A=',
-    'osd': 'VVgTC05r-E0Cjn9mPmQ9m5Iz8JcoNckpOdtNI3s4tic05ksqfevit2iLfm494fTFVbUN2_iCOtFj_iWxob_r-5I=',
-    'l_n_c': '1',
-    'l_cap_id': '"ZTVlYjY2ZTQ4MTIxNGQ3OTkxM2MzZGYwOTZkNWNmZTA=|1710822060|a47581e625628480707aa27ce1bd49aa82e49d53"',
-    'r_cap_id': '"MDIyNjc4NzkyNjQ0NDY0M2I1ZWRmMmQ4ZjE4ZTc5NWM=|1710822060|601d7a9febcdba76b7ebb6b48307a848df2477ae"',
-    'cap_id': '"N2FlYTBkZDMyOWRjNDkwNzlkZDU3NjI1Mzg5MWY1MzE=|1710822060|3dbd5e839b06e6e4ca7fd04ec8f4200db06d5991"',
-    'n_c': '1',
-    'gdxidpyhxdE': 'WGrCKqXjt3C4OvjhaQb30njeIb80A56DQEsrWEbj6W9p%2B1bDkipy6h%2FZ8PgT%2FrZ7%2FHCt98cKQY%2F8Za16HtwjHinj9afwjVfCX9cshMIL0CN%2B%2Fi%5C0yjn4bt7zddgAbfIB8yz7zI3gSZZH6LnlhUHJAZe8vsG4f3x%5CgIjsYac3oCc%2BMKaW%3A1710825812177',
-    'Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49': '1710825137',
-    'captcha_session_v2': '2|1:0|10:1710825135|18:captcha_session_v2|88:WjFwbERVa0wxcllUYjFqYlBkOGRyQVJWMnhTaTZVV2dlVmdKeXFSZW1uNDd2cDRWYytFNnNFdms4T1BYZ2VMaw==|89208309a9073ee582e818d51f09c75abbfdb85b00249d301ee60943d1038c73',
-    'KLBRSID': 'e42bab774ac0012482937540873c03cf|1710825146|1710820686',
+    '_zap': '29b22fff-b6af-4976-a07c-d25650ac4773',
+    'd_c0': 'ADDSD-j4lRePTsK721fRFrWpxQLGjfijbjo=|1697981461',
+    'YD00517437729195%3AWM_TID': 'LuLH7LZ%2FK%2BNBBRERVRLFyyI6032kauLu',
+    '_xsrf': 'obSsNmCr73VUbekodsFvwlVW1ZxbC69C',
+    '__snaker__id': 'OVEO9dZEKxmWl8qP',
+    'YD00517437729195%3AWM_NI': 'CIn38fesHw35YIr0Wf7X8RHzTTtsYUWbcv2UFxl%2BWSsYSPTDfAsTBrKnBaBrusQOww517%2FY%2Fz%2FJQOuDjfoizvouLjp3q%2BpYU5X%2B7tpct01AD6GSqjQA6kwgtAw4bgKeeYkM%3D',
+    'YD00517437729195%3AWM_NIKE': '9ca17ae2e6ffcda170e2e6ee87f780b08fa6d8c95df5bc8fa6c44a968a9b82c462f6f599b0d63d90efa2aff12af0fea7c3b92a8197a2d4bb6ea7ad8898b23f9b97bed1d66ef588f8b3cb72988cbc94dc5f95bead97b15f86a8a4ccd659b1879db1d750a69fbdb5c17a89b28e82c17ab7ad9fb0f67df7eff9d0dc25baf186b2ce7f88b6aa90ec72b6b1bbade97cf1afe591dc5ff788e5d8f77ca1ac00d2c450f6999d92e95b83eaa598e68083a9fc83e641a7bb978cd037e2a3',
+    'q_c1': '0389e54222484ac5a1197c8dd8f94c5a|1703592035000|1703592035000',
+    'z_c0': '2|1:0|10:1709612766|4:z_c0|80:MS4xN0hnQ0lnQUFBQUFtQUFBQVlBSlZUZnY4ejJZUktrTlczNVhFWDB0R1VmWlZvd05CaHlJRGV3PT0=|6a3445892d129423511b31d7242fdae44243cbb3ed5fd78e7da31dbf9667ca47',
+    'tst': 'r',
+    'Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49': '1709978943,1710499034,1710588805,1710891721',
+    'Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49': '1711017673',
+    'KLBRSID': 'd6f775bb0765885473b0cba3a5fa9c12|1711018325|1711017078',
 }
+
+requests.utils.add_dict_to_cookiejar(session.cookies, cookies)
 
 headers = {
     'authority': 'www.zhihu.com',
     'accept': '*/*',
-    'accept-language': 'zh-CN,zh;q=0.9',
-    # 'cookie': '_zap=6cc272a9-fab9-48cd-80bf-abff70c21536; d_c0=AHBWO4qj1xePTmvvvLzm1-P5ya-H2eLVjLI=|1702388268; _xsrf=ilBJqmFgU8UurgfMhq65J4dEu0tegi1I; __snaker__id=61tnc5jXnp6uo3h0; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1710820695; SESSIONID=Y7j6W5CKQIl8e6KEKLplDKox4rqymjIQm4qC2qxojuV; JOID=VVgVC0xr-EsCjH9mOGQ_m5I18JUoNc8pO9tNJXs6ticy5kkqfe3itWiLeG4_4fTDVbcN2_6CONFj-CWzob_t-5A=; osd=VVgTC05r-E0Cjn9mPmQ9m5Iz8JcoNckpOdtNI3s4tic05ksqfevit2iLfm494fTFVbUN2_iCOtFj_iWxob_r-5I=; l_n_c=1; l_cap_id="ZTVlYjY2ZTQ4MTIxNGQ3OTkxM2MzZGYwOTZkNWNmZTA=|1710822060|a47581e625628480707aa27ce1bd49aa82e49d53"; r_cap_id="MDIyNjc4NzkyNjQ0NDY0M2I1ZWRmMmQ4ZjE4ZTc5NWM=|1710822060|601d7a9febcdba76b7ebb6b48307a848df2477ae"; cap_id="N2FlYTBkZDMyOWRjNDkwNzlkZDU3NjI1Mzg5MWY1MzE=|1710822060|3dbd5e839b06e6e4ca7fd04ec8f4200db06d5991"; n_c=1; Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49=1710825209; captcha_session_v2=2|1:0|10:1710825205|18:captcha_session_v2|88:eUdWTENYYVFVVXFMQWdOSm85K21ZcHlpU0R3dllucU5iVW9ySGFKSXpiMGFmVnFkRjFVYkVKcjJ6aFA1VTNyNA==|0d9b8c1e7976ef1f7af29309c0076c660b7bc3d93ca3ec5434dbfdc9b577e43b; gdxidpyhxdE=w6Bxq4OzZWBNsIsx45w7or24VYqQ4tExv%2BPQqCKx2zKsVdU1Throdce5Rzu1B0aNOPShqJV5vGrfClbwnHRVnse%2FT3Bbv2joEbzQyBqXo8zbOvjlyWEL%2By6zvZRr7GbN%2B8gYzzyqtVxAnLyt2Q31%2ByH5onAkQP7O8EiXQ4UBDUV4Mq%2Bn%3A1710843339841; KLBRSID=d1f07ca9b929274b65d830a00cbd719a|1710842467|1710842435',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'cache-control': 'no-cache',
+    # 'cookie': '_zap=29b22fff-b6af-4976-a07c-d25650ac4773; d_c0=ADDSD-j4lRePTsK721fRFrWpxQLGjfijbjo=|1697981461; YD00517437729195%3AWM_TID=LuLH7LZ%2FK%2BNBBRERVRLFyyI6032kauLu; _xsrf=obSsNmCr73VUbekodsFvwlVW1ZxbC69C; __snaker__id=OVEO9dZEKxmWl8qP; YD00517437729195%3AWM_NI=CIn38fesHw35YIr0Wf7X8RHzTTtsYUWbcv2UFxl%2BWSsYSPTDfAsTBrKnBaBrusQOww517%2FY%2Fz%2FJQOuDjfoizvouLjp3q%2BpYU5X%2B7tpct01AD6GSqjQA6kwgtAw4bgKeeYkM%3D; YD00517437729195%3AWM_NIKE=9ca17ae2e6ffcda170e2e6ee87f780b08fa6d8c95df5bc8fa6c44a968a9b82c462f6f599b0d63d90efa2aff12af0fea7c3b92a8197a2d4bb6ea7ad8898b23f9b97bed1d66ef588f8b3cb72988cbc94dc5f95bead97b15f86a8a4ccd659b1879db1d750a69fbdb5c17a89b28e82c17ab7ad9fb0f67df7eff9d0dc25baf186b2ce7f88b6aa90ec72b6b1bbade97cf1afe591dc5ff788e5d8f77ca1ac00d2c450f6999d92e95b83eaa598e68083a9fc83e641a7bb978cd037e2a3; q_c1=0389e54222484ac5a1197c8dd8f94c5a|1703592035000|1703592035000; z_c0=2|1:0|10:1709612766|4:z_c0|80:MS4xN0hnQ0lnQUFBQUFtQUFBQVlBSlZUZnY4ejJZUktrTlczNVhFWDB0R1VmWlZvd05CaHlJRGV3PT0=|6a3445892d129423511b31d7242fdae44243cbb3ed5fd78e7da31dbf9667ca47; tst=r; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1709978943,1710499034,1710588805,1710891721; Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49=1711017673; KLBRSID=d6f775bb0765885473b0cba3a5fa9c12|1711018325|1711017078',
+    'pragma': 'no-cache',
     'referer': 'https://www.zhihu.com/question/616391683/answer/3157488975?utm_psn=1753376002074394624',
-    'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+    'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'user-agent': ua.random,
     'x-requested-with': 'fetch',
     'x-zse-93': '101_3_3.0',
-    'x-zse-96': '2.0_veDZfSyz90Ja4xqjZynrcXWnSyk0mXwJP7dtpDaRiQr=ne3=sxsLD7tzvcnojx',
+    'x-zse-96': '2.0_0QNinW+o2qwzcm2jtQD8wMobKKGaCaOCkV4Pv93X4bCsshYWCM6YnZJyRaJiKeYU',
 }
 
 params = {
@@ -54,15 +65,15 @@ params = {
     'offset': '',
 }
 
-session = requests.session()
-requests.utils.add_dict_to_cookiejar(session.cookies, cookies)
-
 response = session.get(
     'https://www.zhihu.com/api/v4/comment_v5/questions/616391683/root_comment',
     params=params,
     cookies=cookies,
     headers=headers,
 )
+
+if response.cookies.get_dict():        #保持cookie有效
+    session.cookies.update(response.cookies)
 
 result = response.json()
 print(result)
@@ -71,13 +82,17 @@ for item in result.get('data'):
     print(item.get('author').get('name'), item.get('content'))
 
 next_url = result.get('paging').get('next')
-print(next_url)
-time.sleep(1)
-print(ua.edge)
-headers.update({'user-agent': ua.edge,
-                'referer':'https://www.zhihu.com/question/616391683/answer/2111409251'})
-print(headers)
+print('下一页:', next_url)
+
 if next_url:
-    new_response = session.get(next_url, headers=headers, cookies=response.cookies)
+    # print(session.cookies)
+    for item in session.cookies:
+        print(item)
+    # 更新header的 x-zse-96值
+    print("cookies['d_co']", cookies.get('d_c0'))
+    x96 = get_x96(next_url, cookies.get('d_c0'))
+    headers['x-zse-96'] = x96
+    print("headers", headers.get('x-zse-96'))
+    new_response = session.get(next_url, headers=headers)
     print(new_response.cookies)
     print(new_response.json())
