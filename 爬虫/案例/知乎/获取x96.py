61c69c310f33b126fc6e4477569c1e4b0236dade
sunliguo <sunliguo2006@qq.com>
2024-03-21 19:55:04 +0800

    知乎逆向更新

---
+++ 爬虫/案例/知乎/获取x96.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2024-03-21 19:07
+"""
+import execjs
+
+
+def get_x96(tt, tu):
+    with open('demo5.js', encoding='utf-8') as fp:
+        js_content = fp.read()
+
+    jj = execjs.compile(js_content)
+    return jj.call('x93', tt, tu)
+
+
+if __name__ == '__main__':
+    tt = '/api/v4/search/preset_words'
+    tu = 'ADDSD-j4lRePTsK721fRFrWpxQLGjfijbjo=|1697981461'
+    print(get_x96(tt, tu))
