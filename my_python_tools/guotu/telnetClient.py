1cdefbda83e20ec2f3d0dcebc98588059d4f606e
sunliguo <sunliguo2006@qq.com>
2025-01-20 17:35:33 +0800

    telnet客户端更新

--- my_python_tools/guotu/telnetClient.py
+++ my_python_tools/guotu/telnetClient.py
@@ -16,20 +16,41 @@ logging.basicConfig(filename='telnet.log',
 
 
 class TelnetClient(object):
+    """
+    TelnetClient类用于实现Telnet客户端的连接、登录、执行命令和登出功能。
+    """
     def __init__(self):
+        """
+        初始化TelnetClient实例，创建telnetlib.Telnet对象。
+        """
         self.tn = telnetlib.Telnet()
 
     def login_host(self, host_ip, host_port=23, username=None, password=None):
+        """
+          登录Telnet服务器。
+
+          :param host: Telnet服务器的IP地址或主机名
+          :param username: 登录用户名，默认为空
+          :param password: 登录密码，默认为空
+          :return: 如果登录成功返回True，否则返回False
+          """
         try:
             self.tn.open(host_ip, port=host_port)
+            # 根据服务器响应读取提示信息
+            prompt = self.tn.read_until(b'Password:',timeout=10)
+            if username is not None:
+                self.tn.write(username.encode('ascii') + b'\n')
+                prompt = self.tn.read_until(b'Password:').decode('ascii')
+            if password is not None:
+                self.tn.write(password.encode('ascii') + b'\n')
         except Exception as e:
             logging.warning(f"{host_ip}login failed")
             return False
-        self.tn.read_until(b'Password:', timeout=10)
-        self.tn.write(password.encode('ascii') + b'\n')
-        time.sleep(2)
-
-        #判断是否登陆成功
+        # 等待登录后的提示信息，确认是否登录成功
+        self.tn.read_until(b'$ ').decode('utf-8')
+        logging.info(f"Logged in to {host} successfully.")
+        return True
+        # 判断是否登陆成功
         command_result = self.tn.read_very_eager().decode('ascii')
         if '%Username or password is invalid.' not in command_result:
             print("login success")
